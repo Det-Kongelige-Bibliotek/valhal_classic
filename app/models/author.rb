6a91ea3ff9493043ae3c467dd550a017609113d9
Alex Boesen <Hangsolow@server>
2012-11-13 15:01:13 +0100

    fixed small typo in author.rb

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ require 'datastreams/adl_tei_p5'
 require 'datastreams/mods'
 
 class Author < ActiveFedora::Base
-  has_metadata :name=>'descMetadata', :type=>KB::Datastream::Mods
-  has_metadata :name=>'TEI_P5', :type=>
-  delegate :authorName, :to =>''
+  has_metadata :name => 'descMetadata', :type => KB::Datastream::Mods
+  has_metadata :name => 'TEI_P5', :type => KB::Datastream::AdlTeiP5
+
 end
