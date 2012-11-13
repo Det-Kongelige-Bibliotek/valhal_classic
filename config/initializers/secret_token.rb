fa353c14eea629ac8a80f47938bfeac5691cd322
Blimey74 <jatr@kb.dk>
2012-11-13 10:30:10 +0100

    SIFD-2 Creating initial rails project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ADL::Application.config.secret_token = '573ae01590c85da536aa12759f99fb138f822981e99e489d2f2b871bae420cb2910d5f93b7ca8409bafa7db289c1da7ac2d2f4807cf6ebd6bc7b5585e0f1cbbc'
