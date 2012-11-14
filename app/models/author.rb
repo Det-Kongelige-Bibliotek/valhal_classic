9499b889e1cd2fba0c789ab6da1859cd31a6f6c0
Alex Boesen <Hangsolow@server>
2012-11-14 09:54:11 +0100

    have create the basic form on the view

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,8 @@ require 'datastreams/adl_tei_p5'
 require 'datastreams/mods'
 
 class Author < ActiveFedora::Base
+  #Dummy fields that I needed to display the create new author view, must be replaced by real model
+  attr_accessor :life, :name, :quote, :time_line, :external_description, :tei_xml, :picture, :time_line_year, :time_line_description, :external_link
   has_metadata :name => 'descMetadata', :type => KB::Datastream::Mods
   has_metadata :name => 'TEI_P5', :type => KB::Datastream::AdlTeiP5
 
