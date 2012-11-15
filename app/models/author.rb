a00543fcdd0d417507db4052117d19825471d0ef
Alex Boesen <Hangsolow@server>
2012-11-15 12:55:24 +0100

    US338 fixed small bugs in author.js.coffee

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,7 +3,7 @@ require 'datastreams/mods'
 
 class Author < ActiveFedora::Base
   #Dummy fields that I needed to display the create new author view, must be replaced by real model
-  attr_accessor :life, :name, :quote, :time_line, :external_description, :tei_xml, :picture, :time_line_year, :time_line_description, :external_link
+  attr_accessor :life, :name, :quote, :time_line, :external, :tei_xml, :picture, :time_line_year, :time_line_description, :quote_source
   has_metadata :name => 'descMetadata', :type => KB::Datastream::Mods
   has_metadata :name => 'TEI_P5', :type => KB::Datastream::AdlTeiP5
 
