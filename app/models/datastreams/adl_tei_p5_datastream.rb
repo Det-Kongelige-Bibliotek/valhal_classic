b5be7280f326f8dc53b8af9b358b7d9e794e1275
Blimey74 <jatr@kb.dk>
2012-11-20 09:37:09 +0100

    SIFD-2 checking in latest code with changes for handling birth and death

--- app/models/datastreams/adl_tei_p5_datastream.rb
+++ app/models/datastreams/adl_tei_p5_datastream.rb
@@ -2,13 +2,16 @@
 module Datastreams
   class AdlTeiP5Datastream < ActiveFedora::NokogiriDatastream
     set_terminology do |t|
-      t.root(:path=>'TEI', :xmlns=>"http://www.tei-c.org/ns/1.0")
-      t.title(:path=>"teiHeader/oxns:fileDesc/oxns:titleStmt/oxns:title", :index_as=>[:searchable, :facetable])
-      t.surname(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:persName/oxns:surname", :index_as=>[:searchable])
-      t.forename(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:persName/oxns:forename")
-      t.date_of_birth(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:birth/@when")
-      t.date_of_death(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:death/@when")
-      t.sample_quotation(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:note/oxns:cit/oxns:quote/")
+      t.root(:path=>'TEI', :xmlns=>"http://www.tei-c.org/ns/1.0", :schema=>"file:///home/jatr/work/sifd/ADL/spec/fixtures/adl-tei-schema.rng")
+
+      ##t.title(:path=>"teiHeader/oxns:fileDesc/oxns:titleStmt/oxns:title", :index_as=>[:searchable, :facetable])
+      #t.surname(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:persName/oxns:surname", :index_as=>[:searchable])
+      #t.forename(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:persName/oxns:forename")
+      #t.birth(:path=>"string(teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:birth/@when)")
+      #t.birth(:path=>"/*[name()='TEI']/*[name()='teiHeader']/*[name()='profileDesc']/*[name()='particDesc']/*[name()='listPerson']/*[name()='person']/*[name()='birth']/@when")
+      #t.death(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:death/@when")
+      #t.sample_quotation(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:note/oxns:cit/oxns:quote/")
+
       t.teiHeader {
         t.fileDesc {
           t.sourceDesc {
@@ -22,14 +25,27 @@ module Datastreams
                 t.persName {
                   t.surname(:index_as=>[:searchable])
                   t.forename(:index_as=>[:searchable])
-                  t.date_of_birth(:index_as=>[:searchable])
+                }
+                t.birth {
+                  t.date()
+                }
+                t.death{
+                  t.date()
+                }
+                t.event {
+                  t.desc(:index_as=>[:searchable])
+                }
+                t.note {
+                  t.cit{
+                    t.quote(:index_as=>[:searchable])
+                    t.bibl(:index_as=>[:searchable])
+                  }
                 }
               }
             }
           }
         }
       }
-      t.bibl(:proxy=>[:teiHeader, :fileDesc, :sourceDesc, :bibl])
     end
   end
 end
