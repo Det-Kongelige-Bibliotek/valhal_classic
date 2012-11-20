fdf500dfc01698f745b69715701683497918e220
Blimey74 <jatr@kb.dk>
2012-11-20 13:24:22 +0100

    SIFD-2 add self.xml_template for defining an empty TEI record for an ADL author

--- app/models/datastreams/adl_tei_p5_datastream.rb
+++ app/models/datastreams/adl_tei_p5_datastream.rb
@@ -1,16 +1,11 @@
 # -*- encoding : utf-8 -*-
 module Datastreams
   class AdlTeiP5Datastream < ActiveFedora::NokogiriDatastream
-    set_terminology do |t|
-      t.root(:path=>'TEI', :xmlns=>"http://www.tei-c.org/ns/1.0", :schema=>"file:///home/jatr/work/sifd/ADL/spec/fixtures/adl-tei-schema.rng")
 
-      ##t.title(:path=>"teiHeader/oxns:fileDesc/oxns:titleStmt/oxns:title", :index_as=>[:searchable, :facetable])
-      #t.surname(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:persName/oxns:surname", :index_as=>[:searchable])
-      #t.forename(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:persName/oxns:forename")
-      #t.birth(:path=>"string(teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:birth/@when)")
-      #t.birth(:path=>"/*[name()='TEI']/*[name()='teiHeader']/*[name()='profileDesc']/*[name()='particDesc']/*[name()='listPerson']/*[name()='person']/*[name()='birth']/@when")
-      #t.death(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:death/@when")
-      #t.sample_quotation(:path=>"teiHeader/oxns:profileDesc/oxns:particDesc/oxns:listPerson/oxns:person/oxns:note/oxns:cit/oxns:quote/")
+    TEI_NS = 'http://www.tei-c.org/ns/1.0'
+
+    set_terminology do |t|
+      #t.root(:path=>'TEI', :xmlns=>"http://www.tei-c.org/ns/1.0", :schema=>"file:///home/jatr/work/sifd/ADL/spec/fixtures/adl-tei-schema.rng")
 
       t.teiHeader {
         t.fileDesc {
@@ -49,11 +44,59 @@ module Datastreams
     end
 
     def self.xml_template
-      Nokogiri::XML::Builder.new do |xml|
-        xml.root do
-          xml.teiHeader
-        end
-      end.doc
+      Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+        xml.TEI(TEI_NS) {
+          xml.teiHeader {
+            xml.fileDesc {
+              xml.titleStmt {
+                xml.title
+                xml.respStmt {
+                  xml.resp
+                  xml.name
+                }
+                xml.author
+              }
+              xml.publicationStmt {
+                xml.publisher
+                xml.idno
+              }
+              xml.notesStmt {
+                xml.note
+              }
+              xml.sourceDesc {
+                xml.bibl
+              }
+            }
+            xml.profileDesc {
+              xml.particDesc {
+                xml.listPerson {
+                  xml.person {
+                    xml.persName {
+                      xml.surname
+                      xml.forename
+                    }
+                    xml.birth {
+                      xml.date
+                    }
+                    xml.death {
+                      xml.date
+                    }
+                    xml.event {
+                      xml.desc
+                    }
+                    xml.note {
+                      xml.cit {
+                        xml.quote
+                        xml.bibl
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      end
     end
   end
 end
