d0dc25e22536c26bcbca9672a8d8488601a9e3c2
Alex Boesen <hangsolow@server>
2012-11-20 11:14:23 +0100

    added :at attribute to the delegates

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,11 +4,12 @@ class Author < ActiveFedora::Base
 
   has_metadata :name => 'descMetadata', :type => Datastreams::AdlTeiP5Datastream
 
-  delegate :surname, :to =>'descMetadata'
-  delegate :forename, :to =>'descMetadata'
-  delegate :date_of_birth, :to =>'descMetadata'
-  delegate :date_of_death, :to =>'descMetadata'
+  delegate :surname, :to => 'descMetadata', :at => [:teiHeader, :profileDesc, :particDesc, :listPerson, :person, :persName, :surname]
+  delegate :forename, :to => 'descMetadata', :at => [:teiHeader, :profileDesc, :particDesc, :listPerson, :person, :persName, :forename]
+  delegate :date_of_birth, :to => 'descMetadata', :at => [:teiHeader, :profileDesc, :particDesc, :listPerson, :person, :birth, :date]
+  delegate :date_of_death, :to => 'descMetadata', :at => [:teiHeader, :profileDesc, :particDesc, :listPerson, :person, :death, :date]
   delegate :short_bio, :to => 'descMetadata'
   delegate :sample_quotation, :to => 'descMetadata'
   delegate :sample_quotation_source, :to => 'descMetadata'
+
 end
