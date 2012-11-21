6e5c70bace620601fe8b669366906af43d86935b
Alex Boesen <hangsolow@server>
2012-11-21 14:36:29 +0100

    SIFD 2 added test for AuthorsController#index

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,6 +2,18 @@
 require 'spec_helper'
 
 describe AuthorsController do
+  describe "#index" do
+    before do
+      @author = Author.create
+    end
+    it "should be successful" do
+      get :index
+      assigns[:authors].should include @author
+      assigns[:authors].each { |a| a.should be_kind_of Author }
+      response.should be_successful
+    end
+  end
+
   describe "#new" do
     it "should be successful" do
       get :new
@@ -17,7 +29,7 @@ describe AuthorsController do
     end
 
     it "Should create a author" do
-      post :create, :author => {:forename => "Dude",:surname => "Miller"}
+      post :create, :author => {:forename => "Dude", :surname => "Miller"}
       Author.count.should == 1
       response.should redirect_to authors_path
     end
