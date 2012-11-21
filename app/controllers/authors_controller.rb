85ebd9aadf835ee9a9fa6592d2ad1fc21630457f
nader <nkh@kb.dk>
2012-11-21 12:27:38 +0100

    Author Controllers

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,41 +1,58 @@
 # -*- encoding : utf-8 -*-
 class AuthorsController < ApplicationController
-
   def index
-
+    @author = Author.all
   end
 
   def new
     @author = Author.new
   end
 
+  def review
+    @author = Author.new(params[:author])
+    if params[:author][:surname].blank?
+      @author.errors[:surname] = "write Author's surname"
+      render :action=>'new'
+      return
+    end
+    redirect_to author_view_path
+  end
+
   def create
-    @author = Author.new
-    unless params[:author][:upload].blank?
-      content = File.open(params[:author][:upload], 'r:utf-8').read
-      puts content.encoding.name
-      @author.descMetadata.content = content
-    end
-
-    unless params[:author][:forename].blank?
-      @author.forename = params[:author][:forename]
-    end
-
-    unless params[:author][:surname].blank?
-      @author.surname = params[:author][:surname]
-    end
-
-    unless params[:author][:date_of_birth].blank?
-      @author.date_of_birth = params[:author][:date_of_birth]
-    end
-
-    unless params[:author][:date_of_death].blank?
-      @author.date_of_death = params[:author][:date_of_death]
+    @author = Author.new(params[:author])
+    if params[:author][:surname].blank?
+      @author.errors[:surname] = "write Author's surname"
+      render :action=>'new'
+      return
     end
 
     @author.save
-    redirect_to authors_index_path
-
+    redirect_to  author_index_path, :notice=>"Author is saved"
   end
 
+  def publish
+    @author = Author.new(params[:author])
+    if params[:author][:surname].blank?
+      @author.errors[:surname] = "write Author's surname"
+      render :action=>'new'
+      return
+    end
+    @author.save
+    redirect_to author_view_path, :notice=>"Author is saved and published"
+  end
+
+  def add_image
+    @author = Author.new(params[:author])
+    if params[:author][:surname].blank?
+      @author.errors[:surname] = "write Author's surname"
+      render :action=>'new'
+      return
+    end
+    author.image_link =  params[:author][image_link]
+    @author.save
+  end
+
+  def browse
+
+  end
 end
