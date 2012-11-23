a5d6d04f3a55c8109befe25922facbdf10b37be4
Alex Boesen <hangsolow@server>
2012-11-23 12:52:42 +0100

    SIFD-2 Small fix to the authors_spec

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -9,6 +9,8 @@ describe "Authors" do
       visit new_author_path
       fill_in "author_forename", with: "Alex"
       fill_in "author_surname", with: "Boesen"
+      fill_in "author_date_of_birth", with: "1988"
+      fill_in "author_date_of_death", with: "2012"
 
       click_button "Create"
       page.should have_content "forfatter er blevet tilføjet"
@@ -18,10 +20,21 @@ describe "Authors" do
 
   describe "Edit a author" do
     before do
-      Author.create!(forename: "Al", surname: "Bo")
+      Author.all.each { |a| a.delete }
+      @author = Author.create(forename: "Al", surname: "Boesen", date_of_birth: "1988", date_of_death: "2012")
     end
     it "should edit a author" do
-      visit
+      visit authors_path
+      page.should have_content "Al Boesen"
+      click_link "Ændre"
+      fill_in "author_forename", with: "Alex"
+      click_button "Update"
+      page.should have_content "Alex Boesen"
+      page.should have_content "Forfatter er blevet ændret"
     end
   end
+
+
+
+
 end
