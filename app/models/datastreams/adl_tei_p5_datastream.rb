4298c86a402cb36c27c69fd4d270069e653bcadd
Blimey74 <jatr@kb.dk>
2012-11-23 16:42:15 +0100

    SIFD-2 Fixed the model so it reads attributes successfully, need to integrate it with the controller next

--- app/models/datastreams/adl_tei_p5_datastream.rb
+++ app/models/datastreams/adl_tei_p5_datastream.rb
@@ -1,39 +1,42 @@
 # -*- encoding : utf-8 -*-
 module Datastreams
   class AdlTeiP5Datastream < ActiveFedora::NokogiriDatastream
-
-    TEI_NS = 'http://www.tei-c.org/ns/1.0'
+    include OM::XML::Document
 
     set_terminology do |t|
-      t.TEI {
-        t.teiHeader {
-          t.fileDesc {
-            t.sourceDesc {
-              t.bibl(:index_as=>[:searchable])
-            }
+      t.root(:path=>"TEI", :xmlns=>"http://www.tei-c.org/ns/1.0")
+
+      t.teiHeader {
+        t.fileDesc {
+          t.sourceDesc {
+            t.bibl(:index_as=>[:searchable])
           }
-          t.profileDesc {
-            t.particDesc {
-              t.listPerson {
-                t.person {
-                  t.persName {
-                    t.surname(:index_as=>[:searchable])
-                    t.forename(:index_as=>[:searchable])
-                  }
-                  t.birth {
-                    t.date()
-                  }
-                  t.death{
-                    t.date()
-                  }
-                  t.event(:when => '', :type => '', :from => '', :to => '') {
-                    t.desc(:index_as=>[:searchable])
-                  }
-                  t.note {
-                    t.cit{
-                      t.quote(:index_as=>[:searchable])
-                      t.bibl(:index_as=>[:searchable])
-                    }
+        }
+        t.profileDesc {
+          t.particDesc {
+            t.listPerson {
+              t.person {
+                t.persName {
+                  t.surname(:index_as=>[:searchable])
+                  t.forename(:index_as=>[:searchable])
+                }
+                t.birth {
+                  t.date()
+                }
+                t.death{
+                  t.date()
+                }
+              t.event {
+                t.type(:path => {:attribute => "type"})
+                t.when(:path => {:attribute => "when"})
+                t.from(:path => {:attribute => "from"})
+                t.to(:path => {:attribute => "to"})
+                  t.desc(:index_as=>[:searchable])
+                }
+                t.note {
+                  t.cit{
+                    t.quote(:index_as=>[:searchable])
+                    t.bibl(:index_as=>[:searchable])
                   }
                 }
               }
@@ -45,7 +48,7 @@ module Datastreams
 
     def self.xml_template
       builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-        xml.TEI {
+        xml.TEI(:version=>"1.0", "xmlns"=>"http://www.tei-c.org/ns/1.0") {
           xml.teiHeader {
             xml.fileDesc {
               xml.titleStmt {
@@ -87,8 +90,7 @@ module Datastreams
                     xml.note {
                       xml.cit {
                         xml.quote
-                        xml.bibl
-                      }
+                      xml.bibl
                     }
                   }
                 }
@@ -96,6 +98,7 @@ module Datastreams
             }
           }
         }
+      }
       end
       return builder.doc
     end
