74df3cec0aba2cb309c4b47f72b45e53c99971ba
Alex Boesen <hangsolow@server>
2012-11-29 13:42:55 +0100

    removed out comment code from author_spec.rb

--- spec/modules/author_spec.rb
+++ spec/modules/author_spec.rb
@@ -19,18 +19,17 @@ describe Author do
         forename: "Anders",
         date_of_birth: "1587",
         date_of_death: "1637",
-        sample_quotation: "See ud der springer frem den grumme Diur-forskrecker,
-        En Jæger uden Hund blod-gridsk i marken trecker,
-        Nu er hans Klædning hviid, nu guul, nu trøjen sverted,
-        Langøret, øje-glubsk, skarp-kløred, vel-bestierted,
-        Jeg meen den Løve kæk, stormodig ofver alle,
-        Den fleeste Markens Diur maae knælende food-falde.
-        Hans Fødsel selsom er, naar hand til Verden længer,
-        Sit eget Moders Liif, u-mildelig hand sprenger:
-        Af Redsel slet forbaust, half død tre dage slummer,
-        Ved Fadrens skrek'lig brøl til Liif oc Krafter kommer.",
+        sample_quotation:"See ud der springer frem den grumme Diur-forskrecker,
+          En Jæger uden Hund blod-gridsk i marken trecker,
+          Nu er hans Klædning hviid, nu guul, nu trøjen sverted,
+          Langøret, øje-glubsk, skarp-kløred, vel-bestierted,
+          Jeg meen den Løve kæk, stormodig ofver alle,
+          Den fleeste Markens Diur maae knælende food-falde.
+          Hans Fødsel selsom er, naar hand til Verden længer,
+          Sit eget Moders Liif, u-mildelig hand sprenger:
+          Af Redsel slet forbaust, half død tre dage slummer,
+          Ved Fadrens skrek'lig brøl til Liif oc Krafter kommer.",
         sample_quotation_source: "Hexaëmeron i Samlede Skrifter bd. 1, s. 226)",
-        #"portrait_image" => "/images/ff_foto/aoa._foto.jpg",
         short_biography: "1587 	Født i Ærøskøbing
                             1608 	Præst i København
                             1610 	Magister
@@ -50,7 +49,6 @@ describe Author do
     @author.date_of_death.should == attributes_hash[:date_of_death]
     @author.sample_quotation.first.should == attributes_hash[:sample_quotation]
     @author.sample_quotation_source.first.should == attributes_hash[:sample_quotation_source]
-    #@author.portrait_image.should == attributes_hash["portrait_image"]
     @author.short_biography.first.should == attributes_hash[:short_biography]
   end
 
