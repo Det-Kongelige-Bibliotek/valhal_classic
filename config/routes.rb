22c3c3e814d15054a0c76bf386b6bf130d236b7b
Alex Boesen <hangsolow@server>
2012-12-08 13:01:28 +0100

    Added some missing config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # -*- encoding : utf-8 -*-
 ADL::Application.routes.draw do
+  devise_for :users
+
   root :to => "catalog#index"
 
   Blacklight.add_routes(self)
 
-  devise_for :users
+  #devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
