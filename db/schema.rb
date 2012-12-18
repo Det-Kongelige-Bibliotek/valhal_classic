18765ee424e8d4a96552bd23620524c3770f331c
Alex Boesen <hangsolow@server>
2012-12-18 12:28:19 +0100

    changed the test and development database name

--- db/schema.rb
+++ db/schema.rb
@@ -51,8 +51,4 @@ ActiveRecord::Schema.define(:version => 20121208120040) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "intellectual_entities", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 end
