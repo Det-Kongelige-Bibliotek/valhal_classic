6ba4daa159b574527779f7924ae207f7b6c9daf7
Alex Boesen <hangsolow@server>
2012-12-18 11:37:29 +0100

    outcommented hydra access controls in catalog_controller.rb

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -7,12 +7,13 @@ class CatalogController < ApplicationController
   # Extend Blacklight::Catalog with Hydra behaviors (primarily editing).
   include Hydra::Controller::ControllerBehavior
 
+  # Alwb: I have comment out all hydra access controls with "###" because it isnt setup yet
   # These before_filters apply the hydra access controls
-  before_filter :enforce_show_permissions, :only=>:show
+  ###before_filter :enforce_show_permissions, :only=>:show
   # This applies appropriate access controls to all solr queries
-  CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
+  ###CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
   # This filters out objects that you want to exclude from search results, like FileAssets
-  CatalogController.solr_search_params_logic += [:exclude_unwanted_models]
+  ###CatalogController.solr_search_params_logic += [:exclude_unwanted_models]
 
   configure_blacklight do |config|
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
