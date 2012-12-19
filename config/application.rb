c202a4aba9668cfa214ceb25b619313cc79d6ca5
Blimey74 <jatr@kb.dk>
2012-12-19 09:41:26 +0100

    SIFD-5 turn off the precompilation of assets

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module ADL
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true    
+    config.assets.enabled = false
     # Default SASS Configuration, check out https://github.com/rails/sass-rails for details
     config.assets.compress = !Rails.env.development?
 
