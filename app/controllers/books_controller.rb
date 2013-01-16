da110c8fe42d37b0b9d8059a69abd7b26d42dd41
Blimey74 <jatr@kb.dk>
2013-01-16 16:57:15 +0100

    SIFD-6 Adding first attempt at multiple tif file upload for a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,17 +20,33 @@ class BooksController < ApplicationController
     @book = Book.new(params[:book])
 
     if @book.save
-      if !params[:file].blank? && !params[:file][:tei_file].blank?
+      if !params[:file].blank? && !params[:file][:tei_file].blank? || !params[:file][:tiff_file].blank?
         tei = BookTeiRepresentation.new
         tei.save!
-        file = BasicFile.new
-        file.add_file(params[:file][:tei_file])
-        file.container = tei
-        file.save!
-        tei.files << file
+
+        tiff = BookTiffRepresentation.new
+        tiff.save!
+
+        tei_file = BasicFile.new
+        tei_file.add_file(params[:file][:tei_file])
+        tei_file.container = tei
+        tei_file.save!
+        tei.files << tei_file
 
         tei.book = @book
         tei.save!
+
+        params[:file][:tiff_file].each do |f|
+          tiff_file = BasicFile.new
+          tiff_file.add_file(f)
+          puts f.original_filename
+          tiff_file.container = tiff
+          tiff_file.save!
+          tiff.files << tiff_file
+        end
+
+        tiff.book = @book
+        tiff.save!
       end
       redirect_to @book, notice: 'Book was successfully created.'
     else
