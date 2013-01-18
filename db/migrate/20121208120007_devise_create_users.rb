0a15163424a1ab4ae40b7ac6e70ec7dff9ae15d3
naderak <nkh@kb.dk>
2013-01-18 11:58:42 +0100

    Login added

--- db/migrate/20121208120007_devise_create_users.rb
+++ db/migrate/20121208120007_devise_create_users.rb
@@ -19,6 +19,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
+
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
@@ -33,6 +34,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Token authenticatable
       # t.string :authentication_token
 
+      t.string    :name
+      t.string    :pid
 
       t.timestamps
     end
