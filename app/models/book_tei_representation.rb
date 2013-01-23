5e51ed5f8bd92f575b4dc10a735b1214dd183c08
Claus Jensen <cjen@kb.dk>
2013-01-23 16:28:30 +0100

    Moved TEI version to techMetadata

--- app/models/book_tei_representation.rb
+++ app/models/book_tei_representation.rb
@@ -1,13 +1,14 @@
 # -*- encoding : utf-8 -*-
 class BookTeiRepresentation < ActiveFedora::Base
   has_metadata :name => 'rightsMetadata', :type => Hydra::Datastream::RightsMetadata
-  has_metadata :name => 'descMetadata', :type => ActiveFedora::SimpleDatastream do |m|
+  has_metadata :name => 'descMetadata', :type => ActiveFedora::SimpleDatastream
+  has_metadata :name => 'provMetadata', :type => ActiveFedora::SimpleDatastream
+
+  has_metadata :name => 'techMetadata', :type => ActiveFedora::SimpleDatastream do |m|
     m.field "tei_version", :string
   end
 
-  has_metadata :name => 'provMetadata', :type => ActiveFedora::SimpleDatastream
-
-  delegate_to 'descMetadata', [:tei_version], :unique => true
+  delegate_to 'techMetadata', [:tei_version], :unique => true
 
   has_many :files, :class_name => 'BasicFile', :property => :is_part_of
   belongs_to :book, :property => :is_part_of
