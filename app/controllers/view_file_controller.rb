87be470c6cd6746cf4ec80e4793f321773e3b50f
Blimey74 <jatr@kb.dk>
2013-02-06 17:15:36 +0100

    SIFD-9 Adding some exception handling if the file could not be found

--- app/controllers/view_file_controller.rb
+++ app/controllers/view_file_controller.rb
@@ -3,7 +3,12 @@
 #TODO add exception behaviour if a file can't be found
 class ViewFileController < ApplicationController
   def show
-    @basic_file = BasicFile.find(params[:pid])
-    send_data @basic_file.content.content, {:filename => @basic_file.original_filename, :type => @basic_file.mime_type}
+    begin
+      @basic_file = BasicFile.find(params[:pid])
+      send_data @basic_file.content.content, {:filename => @basic_file.original_filename, :type => @basic_file.mime_type}
+    rescue
+      flash[:error] = 'The file you requested could not be found in Fedora!  Please contact your system administrator'
+      redirect_to :back
+    end
   end
 end
