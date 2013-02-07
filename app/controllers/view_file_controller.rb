5e3269d412581006f6a35edbf5834755e814d012
Blimey74 <jatr@kb.dk>
2013-02-07 09:57:58 +0100

    SIFD-9 Refine the exception handling a bit more

--- app/controllers/view_file_controller.rb
+++ app/controllers/view_file_controller.rb
@@ -6,8 +6,13 @@ class ViewFileController < ApplicationController
     begin
       @basic_file = BasicFile.find(params[:pid])
       send_data @basic_file.content.content, {:filename => @basic_file.original_filename, :type => @basic_file.mime_type}
-    rescue
+    rescue ActiveFedora::ObjectNotFoundError => obj_not_found
       flash[:error] = 'The file you requested could not be found in Fedora!  Please contact your system administrator'
+      logger.error obj_not_found.to_s
+      redirect_to :back
+    rescue StandardError => standard_error
+      flash[:error] = 'An error has occurred.  Please contact your system administrator'
+      logger.error standard_error.to_s
       redirect_to :back
     end
   end
