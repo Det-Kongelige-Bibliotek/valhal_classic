2dcbf756540667a63ade4d4d0aeda2d1f03e206d
Blimey74 <jatr@kb.dk>
2013-02-11 10:25:54 +0100

    SIFD-9 previous code didn't work so reverting to the old solution I found, the problem with this one is that you can only clear the file field once, it doesn't work on subsequent clicks.  I've tried a lot of different things to fix this but can't find a solution that works outside jsFiddle so I'm going to leave it like this for now.

--- app/assets/javascripts/book_tiff_representations.js
+++ app/assets/javascripts/book_tiff_representations.js
@@ -1,15 +1,12 @@
 $(document).ready(function(){
 
-    //file_data = $('#file_data'),
-    var clearBn = $('#clear');
+    var file_data = $('#file_data'),
+    clearBn = $('#clear');
 
     clearBn.on('click', function () {
         // Some browsers will actually honor .val('')
         // So I'm adding it back onto the solution
-        file_data = $("#clear");
-        //alert(file_data.val());
         file_data.replaceWith(file_data.val('').clone(true));
-        //alert(file_data.val());
     });
 
 });
