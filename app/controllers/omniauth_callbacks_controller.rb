c98db15df9fef4b22ac824cc423431f6a81b6de0
Blimey74 <jatr@kb.dk>
2013-02-14 15:35:41 +0100

    SIFD-11 changed "sign" to "log"

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env["omniauth.auth"])
 
     if user.persisted?
-      flash.notice = "Signed in as #{user.name}!"
+      flash.notice = "Logged in as #{user.name}!"
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
