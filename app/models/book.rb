7439affa2f6cfcb650f0145285ca414218a317c9
Blimey74 <jatr@kb.dk>
2013-03-01 10:40:42 +0100

    SIFD-35 separating the validation code in a separate validator class for Books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Book < IntellectualEntity
-
+  include ActiveModel::Validations
   has_metadata :name => 'rightsMetadata', :type => Hydra::Datastream::RightsMetadata
   has_metadata :name=>'descMetadata', :type=>Datastreams::BookMods
 
@@ -18,7 +18,7 @@ class Book < IntellectualEntity
 
   validates :title, :presence => true
   validates :isbn, :numericality => true, :allow_blank => true
-  validate :is_duplicate_isbn?
+  validates_with BookValidator
 
   # Determines whether any TEI representations exists.
   def tei_rep?
@@ -28,29 +28,6 @@ class Book < IntellectualEntity
   # Determines whether any TIFF representations exists.
   def hasTiffRep?
     return tif.any?
-  end
-
-  def is_duplicate_isbn?
-    logger.debug ":isbn.blank? = #{isbn.blank?}"
-    logger.debug ":isbn = #{isbn}"
-    if isbn.blank?
-      logger.debug "Returning false as the ISBN is blank"
-      false
-    else
-      if id.eql? "__DO_NOT_USE__"
-        count = ActiveFedora::SolrService.query("isbn_t:#{isbn} AND has_model_s:\"info:fedora/afmodel:Book\"").size
-      else
-        logger.debug "self.id = #{self.id}"
-        logger.debug "self.pid = #{self.pid}"
-        count = ActiveFedora::SolrService.query("isbn_t:#{isbn} AND has_model_s:\"info:fedora/afmodel:Book\" NOT id:\"#{self.id}\"").size
-      end
-      logger.error "duplicate ISBN count = #{count}"
-      if count > 0 then
-        errors.add(:isbn, "cannot be duplicated")
-      #else
-      #  false
-      end
-    end
   end
 
   # Whether any author for this book has been defined.
---
+++ app/validators/book_validator.rb
@@ -0,0 +1,30 @@
+# -*- encoding : utf-8 -*-
+#Validator class for the Book model contains any custom Rails validation
+class BookValidator < ActiveModel::Validator
+
+  #Overridden from ActiveModel::Validator
+  def validate(record)
+    if is_duplicate_isbn?(record.isbn, record.id)
+      record.errors[:isbn] << "cannot be duplicated"
+    end
+  end
+
+  private
+  def is_duplicate_isbn?(isbn, id)
+    logger.debug ":isbn.blank? = #{isbn.blank?}"
+    logger.debug ":isbn = #{isbn}"
+    if isbn.blank?
+      logger.debug "Returning false as the ISBN is blank"
+      false
+    else
+      if id.eql? "__DO_NOT_USE__"
+        count = ActiveFedora::SolrService.query("isbn_t:#{isbn} AND has_model_s:\"info:fedora/afmodel:Book\"").size
+      else
+        count = ActiveFedora::SolrService.query("isbn_t:#{isbn} AND has_model_s:\"info:fedora/afmodel:Book\" NOT id:\"#{id}\"").size
+      end
+      logger.error "duplicate ISBN count = #{count}"
+      count > 0
+    end
+  end
+
+end
