e398559d9a18e78ae1169e4e21c0b1bb83cf68a8
jolf <jolf@kb.dk>
2013-04-25 13:48:44 +0200

    Fixed unit-tests

--- spec/models/integration/book_and_person_spec.rb
+++ spec/models/integration/book_and_person_spec.rb
@@ -10,7 +10,7 @@ describe "Person and Book" do
       @person = Person.create(:firstname=>"some name", :lastname=>"some lastname", :date_of_birth => Time.new.to_i.to_s)
       @book = Book.create(:title=>"some title")
       @book.save!
-      @person.authored_books << @book
+      @person.authored_manifestations << @book
       @person.save!
       @book.authors << @person
       @book.save!
@@ -20,10 +20,10 @@ describe "Person and Book" do
     end
 
     it "should be defined in the Person entity" do
-      @person.authored_books.should_not be_nil
-      @person.authored_books.should_not be_empty
-      @person.authored_books.length.should == 1
-      @person.authored_books.first.should == @book
+      @person.authored_manifestations.should_not be_nil
+      @person.authored_manifestations.should_not be_empty
+      @person.authored_manifestations.length.should == 1
+      @person.authored_manifestations.first.should == @book
     end
 
     it "should be defined in the Book entity" do
@@ -40,7 +40,7 @@ describe "Person and Book" do
       @person = Person.new(:firstname=>"some name", :lastname=>"some lastname", :date_of_birth => Time.new.to_i.to_s)
       @book = Book.new(:title=>"book with representation")
 
-      @person.authored_books << @book
+      @person.authored_manifestations << @book
       @book.authors << @person
 
       @ptr = DefaultRepresentation.new
@@ -56,10 +56,10 @@ describe "Person and Book" do
     end
 
     it "should be defined in the Person entity" do
-      @person.authored_books.should_not be_nil
-      @person.authored_books.should_not be_empty
-      @person.authored_books.length.should == 1
-      @person.authored_books.first.should == @book
+      @person.authored_manifestations.should_not be_nil
+      @person.authored_manifestations.should_not be_empty
+      @person.authored_manifestations.length.should == 1
+      @person.authored_manifestations.first.should == @book
 
       @person.tei.should_not be_nil
       @person.tei.should_not be_empty
@@ -91,12 +91,12 @@ describe "Person and Book" do
 
     it "should have relationship from the PersonTeiRepresentations point of view" do
       @ptr.person.should == @person
-      @ptr.person.authored_books.should_not be_nil
-      @ptr.person.authored_books.length.should == 1
-      @ptr.person.authored_books.first.should == @book
-      @ptr.person.authored_books.first.tei.should_not be_nil
-      @ptr.person.authored_books.first.tei.length.should == 1
-      @ptr.person.authored_books.first.tei.first.should == @btr
+      @ptr.person.authored_manifestations.should_not be_nil
+      @ptr.person.authored_manifestations.length.should == 1
+      @ptr.person.authored_manifestations.first.should == @book
+      @ptr.person.authored_manifestations.first.tei.should_not be_nil
+      @ptr.person.authored_manifestations.first.tei.length.should == 1
+      @ptr.person.authored_manifestations.first.tei.first.should == @btr
     end
   end
 
@@ -109,31 +109,31 @@ describe "Person and Book" do
     it "should be able to have many authors" do
       person1 = Person.create(:firstname=>"first person", :lastname=>"first lastname", :date_of_birth => Time.new.to_i.to_s)
       person1.save!
-      person1.authored_books << @book
+      person1.authored_manifestations << @book
       person1.save!
       @book.authors << person1
 
       person2 = Person.create(:firstname=>"second person", :lastname=>"first lastname", :date_of_birth => Time.new.to_i.to_s)
       person2.save!
-      person2.authored_books << @book
+      person2.authored_manifestations << @book
       person2.save!
       @book.authors << person2
 
       person3 = Person.create(:firstname=>"third person", :lastname=>"first lastname", :date_of_birth => Time.new.to_i.to_s)
       person3.save!
-      person3.authored_books << @book
+      person3.authored_manifestations << @book
       person3.save!
       @book.authors << person3
 
       person4 = Person.create(:firstname=>"fourth person", :lastname=>"first lastname", :date_of_birth => Time.new.to_i.to_s)
       person4.save!
-      person4.authored_books << @book
+      person4.authored_manifestations << @book
       person4.save!
       @book.authors << person4
 
       person5 = Person.create(:firstname=>"fifth person", :lastname=>"first lastname", :date_of_birth => Time.new.to_i.to_s)
       person5.save!
-      person5.authored_books << @book
+      person5.authored_manifestations << @book
       person5.save!
       @book.authors << person5
 
