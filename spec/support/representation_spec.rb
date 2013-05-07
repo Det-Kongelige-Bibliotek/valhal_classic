1d5c098ab6e9d88dff4a54c7a4d28222e34846e0
naderak <nkh@kb.dk>
2013-05-07 09:42:38 +0200

    adding rspec test

---
+++ spec/support/representation_spec.rb
@@ -0,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+class Representation < ActiveFedora::Base
+  include Concerns::Representation
+end
+
+describe "representation" do
+
+  it "should " do
+
+  end
+end
