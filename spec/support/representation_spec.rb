5e2ffb3a56f81e828320892219fe01470c23f90e
naderak <nkh@kb.dk>
2013-05-07 12:29:55 +0200

    adding rspec test2

--- spec/support/representation_spec.rb
+++ spec/support/representation_spec.rb
@@ -7,7 +7,48 @@ end
 
 describe "representation" do
 
-  it "should " do
-
+  it "should create a representation with label" do
+    represent = Representation.new(:label=>"new label")
+    represent.save.should == true
+    represent.label.should == "new label"
   end
+
+  it "should create a representation without label" do
+    represent = Representation.new
+    represent.save.should == true
+  end
+
+  it "should create a representation without label and make a label for it" do
+    represent = Representation.new
+    represent.save!
+    represent.label.should_not be_nil
+  end
+
+  it "should create a representation and make a label for it even though a blank label is assigned" do
+    represent = Representation.new(:label=>"")
+    represent.label.should == ""
+    represent.save!
+    represent.label.should_not be_nil
+  end
+
+  it "should be able to update a label" do
+    @represent1 = Representation.new(:label=>"first label")
+    @represent1.save!
+    represent2 = Representation.find(@represent1.pid)
+    represent2.label = "second label"
+    represent2.save!
+    represent2.label.should == "second label"
+  end
+
+  it "should be able to delete a representation" do
+    @represent = Representation.new
+    @represent.save!
+    count = Representation.count
+    @represent.destroy
+    Representation.count.should == count - 1
+  end
+
+
+
+
 end
