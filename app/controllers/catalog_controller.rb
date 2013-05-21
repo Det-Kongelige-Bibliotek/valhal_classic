d8cb2f3c4c551733d5a0b350f75015abf6d3c629
Jonas Lindberg Frellesen <jolf@kb.dk>
2013-05-21 10:46:03 +0200

    Fixed another bad commit

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,32 +1,66 @@
 # -*- encoding : utf-8 -*-
 require 'blacklight/catalog'
 
-class CatalogController < ApplicationController  
+class CatalogController < ApplicationController
+  #You don't need login to view catalog
+  skip_authorization_check
+
 
   include Blacklight::Catalog
+  # Extend Blacklight::Catalog with Hydra behaviors (primarily editing).
   include Hydra::Controller::ControllerBehavior
+
+  # Alwb: I have comment out all hydra access controls with "###" because it isnt setup yet
   # These before_filters apply the hydra access controls
-  before_filter :enforce_show_permissions, :only=>:show
+  ###before_filter :enforce_show_permissions, :only=>:show
   # This applies appropriate access controls to all solr queries
-  CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
+  ###CatalogController.solr_search_params_logic += [:add_access_controls_to_solr_params]
   # This filters out objects that you want to exclude from search results, like FileAssets
   CatalogController.solr_search_params_logic += [:exclude_unwanted_models]
 
+  def exclude_unwanted_models(solr_parameters, user_parameters)
+    solr_parameters[:fq] ||= []
+    unwanted_models.each do |model|
+      solr_parameters[:fq] << "-has_model_ssim:\"#{model.to_class_uri}\""
+    end
+  end
+
+  def unwanted_models
+    [BookTiffRepresentation, BookTeiRepresentation ,BasicFile, TiffFile, DefaultRepresentation, OrderedRepresentation, SingleFileRepresentation]
+  end
+
+
 
   configure_blacklight do |config|
+    ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
     config.default_solr_params = { 
       :qt => 'search',
       :rows => 10 
     }
 
-    # solr field configuration for search results/index views
-    config.index.show_link = 'title_tesim'
-    config.index.record_tsim_type = 'has_model_ssim'
+    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
+    ## parameters included in the Blacklight-jetty document requestHandler.
+    #
+    #config.default_document_solr_params = {
+    #  :qt => 'document',
+    #  ## These are hard-coded in the blacklight 'document' requestHandler
+    #  # :fl => '*',
+    #  # :rows => 1
+    #  # :q => '{!raw f=id v=$id}' 
+    #}
 
+    # solr field configuration for search results/index views
+    person_solr_names = Person.solr_names
+    book_solr_names = Book.solr_names
+    config.index.show_link = person_solr_names[:search_result_title]
+    config.index.record_display_type = 'format'
+
+    config.show.html_title = person_solr_names[:person_name]
+    config.show.heading = person_solr_names[:person_name]
     # solr field configuration for document/show views
-    config.show.html_title = 'title_tesim'
-    config.show.heading = 'title_tesim'
-    config.show.display_type = 'has_model_ssim'
+    config.show.html_title = book_solr_names[:title]
+    config.show.heading = book_solr_names[:title]
+    config.show.display_type = 'format'
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
@@ -35,7 +69,7 @@ class CatalogController < ApplicationController
     # * If left unset, then all facet values returned by solr will be displayed.
     # * If set to an integer, then "f.somefield.facet.limit" will be added to
     # solr request, with actual solr request being +1 your configured limit --
-    # you configure the number of items you actually want _tsimed_ in a page.    
+    # you configure the number of items you actually want _displayed_ in a page.    
     # * If set to 'true', then no additional parameters will be sent to solr,
     # but any 'sniffed' request limit parameters will be used for paging, with
     # paging at requested limit -1. Can sniff from facet.limit or 
@@ -47,50 +81,40 @@ class CatalogController < ApplicationController
     #
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
-    config.add_facet_field solr_name('object_type', :facetable), :label => 'Format' 
-    config.add_facet_field solr_name('pub_date', :facetable), :label => 'Publication Year' 
-    config.add_facet_field solr_name('subject_topic', :facetable), :label => 'Topic', :limit => 20 
-    config.add_facet_field solr_name('language', :facetable), :label => 'Language', :limit => true 
-    config.add_facet_field solr_name('lc1_letter', :facetable), :label => 'Call Number' 
-    config.add_facet_field solr_name('subject_geo', :facetable), :label => 'Region' 
-    config.add_facet_field solr_name('subject_era', :facetable), :label => 'Era'  
+    config.add_facet_field 'mime_type_t', :label => 'Format'
+    config.add_facet_field person_solr_names[:person_name], :label => 'Author', :sort => 'index'
+    config.add_facet_field book_solr_names[:title], :label => 'Title', :sort => 'index'
+
+    config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
+
+    config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
+       :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
+       :years_10 => { :label => 'within 10 Years', :fq => "pub_date:[#{Time.now.year - 10 } TO *]" },
+       :years_25 => { :label => 'within 25 Years', :fq => "pub_date:[#{Time.now.year - 25 } TO *]" }
+    }
+
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
     # handler defaults, or have no facets.
-    config.default_solr_params[:'facet.field'] = config.facet_fields.keys
-    #use this instead if you don't want to query facets marked :show=>false
-    #config.default_solr_params[:'facet.field'] = config.facet_fields.select{ |k, v| v[:show] != false}.keys
-
+    config.add_facet_fields_to_solr_request!
 
     # solr fields to be displayed in the index (search results) view
-    #   The ordering of the field names is the order of the display 
-    config.add_index_field solr_name('title', :stored_searchable, type: :string), :label => 'Title:' 
-    config.add_index_field solr_name('title_vern', :stored_searchable, type: :string), :label => 'Title:' 
-    config.add_index_field solr_name('author', :stored_searchable, type: :string), :label => 'Author:' 
-    config.add_index_field solr_name('author_vern', :stored_searchable, type: :string), :label => 'Author:' 
-    config.add_index_field solr_name('format', :symbol), :label => 'Format:' 
-    config.add_index_field solr_name('language', :stored_searchable, type: :string), :label => 'Language:'
-    config.add_index_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_index_field solr_name('published_vern', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_index_field solr_name('lc_callnum', :stored_searchable, type: :string), :label => 'Call number:'
+    #   The ordering of the field names is the order of the display
+    config.add_index_field 'surname_t', :label => 'Lastname:'
+    config.add_index_field 'forename_t', :label => 'Firstname:'
+    config.add_index_field 'birth_date_t', :label => 'Date of Birth:'
+    config.add_index_field 'death_date_t', :label => 'Date of Death:'
+    config.add_index_field 'original_filename_t', :label => 'Name:'
+    #config.add_index_field 'title_t', :label => 'Titel:'
+    #config.add_index_field 'person_name_t', :label => 'Person Name:'
+    config.add_index_field book_solr_names[:search_results_book_authors], :label => 'Author(s):'
 
     # solr fields to be displayed in the show (single result) view
-    #   The ordering of the field names is the order of the display 
-    config.add_show_field solr_name('title', :stored_searchable, type: :string), :label => 'Title:' 
-    config.add_show_field solr_name('title_vern', :stored_searchable, type: :string), :label => 'Title:' 
-    config.add_show_field solr_name('subtitle', :stored_searchable, type: :string), :label => 'Subtitle:' 
-    config.add_show_field solr_name('subtitle_vern', :stored_searchable, type: :string), :label => 'Subtitle:' 
-    config.add_show_field solr_name('author', :stored_searchable, type: :string), :label => 'Author:' 
-    config.add_show_field solr_name('author_vern', :stored_searchable, type: :string), :label => 'Author:' 
-    config.add_show_field solr_name('format', :symbol), :label => 'Format:' 
-    config.add_show_field solr_name('url_fulltext_tsim', :stored_searchable, type: :string), :label => 'URL:'
-    config.add_show_field solr_name('url_suppl_tsim', :stored_searchable, type: :string), :label => 'More Information:'
-    config.add_show_field solr_name('language', :stored_searchable, type: :string), :label => 'Language:'
-    config.add_show_field solr_name('published', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_show_field solr_name('published_vern', :stored_searchable, type: :string), :label => 'Published:'
-    config.add_show_field solr_name('lc_callnum', :stored_searchable, type: :string), :label => 'Call number:'
-    config.add_show_field solr_name('isbn', :stored_searchable, type: :string), :label => 'ISBN:'
+    #   The ordering of the field names is the order of the display
+    config.add_show_field book_solr_names[:title], :label => 'Title:'
+    config.add_show_field person_solr_names[:person_name], :label => 'Person Name:'
+    #config.add_show_field 'description_t', :label => 'Description:'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -118,6 +142,9 @@ class CatalogController < ApplicationController
     # of Solr search fields. 
     
     config.add_search_field('title') do |field|
+      # solr_parameters hash are sent to Solr as ordinary url query params. 
+      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
@@ -129,16 +156,18 @@ class CatalogController < ApplicationController
     end
     
     config.add_search_field('author') do |field|
-      field.solr_local_parameters = { 
+      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+      field.solr_local_parameters = {
         :qf => '$author_qf',
         :pf => '$author_pf'
       }
     end
-    
+
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as 
     # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
     config.add_search_field('subject') do |field|
+      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
       field.qt = 'search'
       field.solr_local_parameters = { 
         :qf => '$subject_qf',
@@ -150,16 +179,13 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_dtsi desc, title_tesi asc', :label => 'relevance'
-    config.add_sort_field 'pub_date_dtsi desc, title_tesi asc', :label => 'year'
-    config.add_sort_field 'author_tesi asc, title_tesi asc', :label => 'author'
-    config.add_sort_field 'title_tesi asc, pub_date_dtsi desc', :label => 'title'
+    #config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', :label => 'relevance'
+    #config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
+    #config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
+    #config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
 
     # If there are more than this many search results, no spelling ("did you 
     # mean") suggestion is offered.
     config.spell_max = 5
   end
-
-
-
-end 
+end
