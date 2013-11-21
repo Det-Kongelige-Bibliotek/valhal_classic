0aeceff6c4a247a801361baeb9cc948df77b5634
Jonas Lindberg Frellesen <jolf@kb.dk>
2013-11-21 08:57:58 +0100

    Changed the 'delegate' and 'delegate_to' to the new hydra 'has_attributes'.

--- app/models/concerns/basic_file.rb
+++ app/models/concerns/basic_file.rb
@@ -17,21 +17,22 @@ module Concerns
       # a ActiveFedora::SimpleDatastream for the techMetadata
       # TODO find out if we need to make our own Datastream for techMetadata
       has_metadata :name => 'techMetadata', :type => ActiveFedora::SimpleDatastream do |m|
-        m.field "file_checksum", :string
-        m.field "original_filename", :string
-        m.field "mime_type", :string
-        m.field "file_size", :integer
-        m.field "last_modified", :string
-        m.field "created", :string
-        m.field "last_accessed", :string
-        m.field "file_uuid", :string
+        m.field 'file_checksum', :string
+        m.field 'original_filename', :string
+        m.field 'mime_type', :string
+        m.field 'file_size', :integer
+        m.field 'last_modified', :string
+        m.field 'created', :string
+        m.field 'last_accessed', :string
+        m.field 'file_uuid', :string
       end
 
-      delegate_to 'techMetadata', [:last_modified, :created, :last_accessed, :original_filename, :mime_type, :file_uuid], :multiple => false
-      delegate_to 'descMetadata', [:description], :multiple => false
+      has_attributes :last_modified, :created, :last_accessed, :original_filename, :mime_type, :file_uuid,
+                     datastream: 'techMetadata', :multiple => false
+      has_attributes :description, datastream: 'descMetadata', :multiple => false
       # TODO have more than one checksum (both MD5 and SHA), and specify their checksum algorithm.
-      delegate :checksum, :to => 'techMetadata', :at => [:file_checksum], :multiple => false
-      delegate :size, :to => 'techMetadata', :at => [:file_size], :multiple => false
+      has_attributes :checksum, datastream: 'techMetadata', :at => [:file_checksum], :multiple => false
+      has_attributes :size, datastream: 'techMetadata', :at => [:file_size], :multiple => false
     end
 
     # adds a content datastream to the object and generate techMetadata for the basic_files
--- app/models/ordered_representation.rb
+++ app/models/ordered_representation.rb
@@ -9,5 +9,5 @@ class OrderedRepresentation < ActiveFedora::Base
   has_metadata :name => 'rightsMetadata', :type => Hydra::Datastream::RightsMetadata
   has_metadata :name => 'techMetadata',   :type => Datastreams::MetsStructMap
 
-  delegate_to 'techMetadata', [:div, :order, :fptr, :file_id], :multiple => false
+  has_attributes :div, :order, :fptr, :file_id, datastream: 'techMetadata', :multiple => false
 end
