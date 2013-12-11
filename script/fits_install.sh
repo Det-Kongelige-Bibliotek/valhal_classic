5d4c735fd7e4b82c12d011dc152458e5ff09c8f7
jatr <jatr@kb.dk>
2013-12-11 14:02:36 +0100

    Make fits.sh executable and make sure to add it to the end of the env variable

--- script/fits_install.sh
+++ script/fits_install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget http://fits.googlecode.com/files/fits-0.6.2.zip
 unzip fits-0.6.2.zip
-export FITS_HOME=`readlink -f fits-0.6.2`
+chmod +x ./fits-0.6.2/fits.sh
+export FITS_HOME=`readlink -f fits-0.6.2/fits.sh`
 echo 'FITS_HOME='${FITS_HOME}
 rm fits-0.6.2.zip
