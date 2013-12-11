fc5618b98b48bde38a3f5641d5066bfcfdbd2076
jatr <jatr@kb.dk>
2013-12-11 13:27:44 +0100

    Set FITS_HOME to full path of the file

--- script/fits_install.sh
+++ script/fits_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget http://fits.googlecode.com/files/fits-0.6.2.zip
 unzip fits-0.6.2.zip
-export FITS_HOME=./fits-0.6.2
+export FITS_HOME=`readlink -f fits-0.6.2`
 echo 'FITS_HOME='${FITS_HOME}
 rm fits-0.6.2.zip
