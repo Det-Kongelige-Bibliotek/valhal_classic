97e1b04027f6c07180de3fd7832457d0e91686a1
jatr <jatr@kb.dk>
2014-01-29 13:46:12 +0100

    Get the latest tag for the release version label

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-APP_VERSION = `git describe --abbrev=0` unless defined? APP_VERSION
+APP_VERSION = `git describe --tags --abbrev=0` unless defined? APP_VERSION
 
 ActionView::Base.field_error_proc = Proc.new {|html, instance| html }
 
