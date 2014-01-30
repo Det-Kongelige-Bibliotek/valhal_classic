40d38b639dd27adfa8683f6fc1011485c62e488b
jatr <jatr@kb.dk>
2014-01-30 10:32:37 +0100

    First shot at improving rspec test performance, removing rspec from under the support directory as there shouldn't  be any tests under the support directory.  Putting them there causes tests to be duplicated and run multiple times slowing the test run by several minutes.

--- spec/models/basic_file_spec.rb
+++ spec/models/basic_file_spec.rb
@@ -159,7 +159,92 @@ describe BasicFile do
 
     it "should return false when a object doesn't support the require methods is passed down" do
 
+    it 'should have the same basic_files-type as mime-type' do
+      @basic_file.file_type.should == @basic_file.mime_type
+    end
+
+    it 'should have a fitsMetadataDatastream with valid content regarding the file metadata' do
+      @basic_file.datastreams['fitsMetadata1'].should_not be_nil
+      @basic_file.datastreams['fitsMetadata1'].content.should_not be_nil
+      expect(@basic_file.datastreams['fitsMetadata1'].content).to include('<metadata>
+    <image>
+      <compressionScheme toolname="Exiftool" toolversion="9.06" status="SINGLE_RESULT">Deflate/Inflate</compressionScheme>
+      <imageWidth toolname="Exiftool" toolversion="9.06" status="SINGLE_RESULT">50</imageWidth>
+      <imageHeight toolname="Exiftool" toolversion="9.06" status="SINGLE_RESULT">64</imageHeight>
+      <scanningSoftwareName toolname="Exiftool" toolversion="9.06" status="SINGLE_RESULT">Adobe ImageReady</scanningSoftwareName>
+    </image>
+  </metadata>')
+    end
+  end
+=begin
+  context "with a very big pdf file" do
+    before do
+      @basic_file = BasicFile.new
+      @uploaded_file = ActionDispatch::Http::UploadedFile.new(filename: 'rails.png', type: 'image/png', tempfile: File.new("#{Rails.root}/spec/fixtures/programming-ruby-1-9-2-0_p1_0.pdf"))
+      @basic_file.add_file(@uploaded_file, nil)
+    end
+
+    it "should have shorthand for adding a basic_files" do
+      file = BasicFile.new
+      file.add_file(@uploaded_file, nil).should be_true
+    end
+
+    it "should have mime type" do
+      @basic_file.mime_type.should == "image/png"
+    end
+
+    it "should have the original filename" do
+      @basic_file.original_filename.should == "rails.png"
+    end
+
+    it "should have the checksum of the basic_files" do
+      @basic_file.checksum.should == "a8ed2f03626c51c814b6f4df250d97b2"
+    end
+
+    it "should have a uuid" do
+      @basic_file.save!
+      @basic_file.uuid.should_not be_nil
+    end
+
+    it "should be able to be saved in repository with a basic_files attended" do
+      @basic_file.save.should be_true
+    end
+
+    it "should have the basic_files size" do
+      @basic_file.size.should == 9782105
+    end
+
+    it "should have a description" do
+      @basic_file.description = "description"
+      @basic_file.description.should == "description"
+    end
+
+    it "should be able to retrive a saved object from the repository" do
+      @basic_file.save
+
+      file = BasicFile.find(@basic_file.pid)
+      file.should == @basic_file
+    end
+
+    it "should have a descMetadata datastream" do
+      @basic_file.descMetadata.should be_kind_of ActiveFedora::Datastream
+    end
+
+    it "should have a techMetadata datastream" do
+      @basic_file.techMetadata.should be_kind_of ActiveFedora::Datastream
+    end
+
+    it "should have a content datastream" do
+      @basic_file.content.should be_kind_of ActiveFedora::Datastream
+    end
+
+    it "should have a rightsMetadata datastream" do
+      @basic_file.rightsMetadata.should be_kind_of Hydra::Datastream::RightsMetadata
+    end
+
+    it "should return false when a object doesn't support the require methods is passed down" do
       @basic_file.add_file("basic_files", nil).should be_false
     end
+=end
   end
 end
--- spec/models/intellectual_entity_spec.rb
+++ spec/models/intellectual_entity_spec.rb
@@ -43,8 +43,46 @@ describe IntellectualEntity do
       @intellectual_entity.save!
       @intellectual_entity.uuid.should == "1234-5678-1234-5768"
     end
+
+    it "should be valid with 16 =< uuid_length =<64" do
+      @intellectual_entity = IntellectualEntity.new
+      @intellectual_entity.uuid = "abcdabcdabcdabcdh"
+      @intellectual_entity.should be_valid
+      @intellectual_entity.save.should be_true
+      @intellectual_entity.uuid.should == "abcdabcdabcdabcdh"
+    end
+
+    it "should not be valid with uuid_length less then 16" do
+      @intellectual_entity = IntellectualEntity.new
+      @intellectual_entity.uuid = "abcdabcd"
+      @intellectual_entity.should_not be_valid
+      @intellectual_entity.save.should be_false
+    end
+
+    it "should not be valid with uuid_length more then 64" do
+      @intellectual_entity = IntellectualEntity.new
+      @intellectual_entity.uuid = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
+      @intellectual_entity.should_not be_valid
+      @intellectual_entity.save.should be_false
+    end
+
+    it "should generate UUID automatically if there is no UUID" do
+      @intellectual_entity = IntellectualEntity.new
+      @intellectual_entity.uuid.should be_nil
+      @intellectual_entity.save!
+      @intellectual_entity.uuid.should be_kind_of String
+    end
+
+    it "should be possible to update UUID" do
+      @intellectual_entity = IntellectualEntity.new
+      @intellectual_entity.uuid = "abcdabcdabcdabcd"
+      @intellectual_entity.save!
+      @intellectual_entity.uuid = "1234123412341234"
+      @intellectual_entity.save!
+      @intellectual_entity.uuid.should == "1234123412341234"
+    end
   end
- 
+
   after do
     IntellectualEntity.all.each do |ie|
       ie.delete
