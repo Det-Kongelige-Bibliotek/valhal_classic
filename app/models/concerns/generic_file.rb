7408a1b384c65a1ad82285b97f9f7b8a37f15cfa
Ronan McHugh <mchugh.r@gmail.com>
2014-02-19 13:52:00 +0100

    we don't need all these log messages.

--- app/models/concerns/generic_file.rb
+++ app/models/concerns/generic_file.rb
@@ -68,23 +68,18 @@ module Concerns
       rescue Hydra::FileCharacterization::ToolNotFoundError => tnfe
         logger.error tnfe.to_s
         logger.error 'Tool for extracting FITS metadata not found, check FITS_HOME environment variable is set and valid installation of fits is present'
-        logger.error 'Continuing with normal processing...'
-        puts tnfe.to_s
-        puts 'Tool for extracting FITS metadata not found, check FITS_HOME environment variable is set and valid installation of fits is present'
+        logger.info 'Continuing with normal processing...'
         return
       rescue RuntimeError => re
         logger.error 'Something went wrong with extraction of file metadata using FITS'
         logger.error re.to_s
-        logger.error 'Continuing with normal processing...'
-        puts re.to_s
-        puts 'Something went wrong with extraction of file metadata using FITS'
+        logger.info 'Continuing with normal processing...'
         if re.to_s.include? "command not found" #if for some reason the fits command cannot be run from the shell, this hack will get round it
           fits_home = `locate fits.sh`.rstrip
           `export FITS_HOME=#{fits_home}`
           stdin, stdout, stderr = Open3.popen3("#{fits_home} -i #{file.path}")
           fits_meta_data = String.new
           stdout.each_line {|line| fits_meta_data.concat(line)}
-          logger.debug "fitsMetadata = #{fits_meta_data}"
         else
           return
         end
