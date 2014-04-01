533abd6e8796faa99efb6d0d1901e48c280d5387
jatr <jatr@kb.dk>
2014-04-01 13:10:23 +0200

    Rspec view test for checking shelf locator appears on views

--- spec/views/works/show.html.erb_spec.rb
+++ spec/views/works/show.html.erb_spec.rb
@@ -1,9 +1,25 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
 
-describe "works/show" do
+describe 'works/show' do
+
   before(:each) do
-    @work = assign(:work, stub_model(Work))
+
+    #Set up a dod_book_mock Work object to test with the view
+    dod_book_mock = mock_model('Work', :work_type => 'DOD eBook', :shelfLocator => '37,-376 8°', :title => 'Test title',
+                  :subTitle => 'test subtitle', :has_author? => false, :has_concerned_person? => false,
+                  :typeOfResource => 'DOD eBook', :publisher => 'test publisher', :originPlace => 'København',
+                  :languageISO => 'da', :languageText => 'da', :subjectTopic => '', :dateIssued=> '',
+                  :physicalExtent => '', :uuid => '', :has_rep? => false)
+
+    assign(:work, dod_book_mock)
+
+  end
+
+  it 'should display shelf location for a work' do
+    render
+    rendered.to_s
+    expect(rendered).to include('37,-376 8°')
   end
 
 end
