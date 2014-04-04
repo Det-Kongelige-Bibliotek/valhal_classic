e5ce012bea7c51908aa409dc8df70c575fdfaab1
jatr <jatr@kb.dk>
2014-04-04 10:50:10 +0200

    Fix for unexpected format aleph sys numbers plus test and fixture

--- app/services/aleph_service.rb
+++ app/services/aleph_service.rb
@@ -26,7 +26,7 @@ class AlephService
   def convert_marc_to_message(marcxml)
       begin
       xml = Nokogiri::XML.parse(marcxml)
-      sys_num = xml.css('present record metadata oai_marc varfield[id="001"]').text.strip[4..12]
+      sys_num = xml.xpath('/present/record/metadata/oai_marc/varfield[@id="001"]/subfield[@label="a"]/text()').to_s.gsub!(/[a-zA-Z]/, '')
       fileUri = xml.css('present record metadata oai_marc varfield[id="URL"] subfield[label="u"]').first.text.strip
       {id: sys_num, fileUri: fileUri, workflowId: 'DOD'}.to_json
     rescue => e
