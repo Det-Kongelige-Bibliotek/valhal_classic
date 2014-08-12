9354d03eab612bfef55b61b0fee8e5dc292dbabd
Jonas Lindberg Frellesen <jolf@kb.dk>
2014-08-12 10:54:27 +0200

    Added administrative metadata

---
+++ app/models/datastreams/admin_datastream.rb
@@ -0,0 +1,30 @@
+# -*- encoding : utf-8 -*-
+module Datastreams
+
+  # Datastream for the administrative metadata for a Work and Instance.
+  class AdminDatastream < ActiveFedora::OmDatastream
+    set_terminology do |t|
+      t.root(:path=>'fields')
+      t.collection(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                :path=>'admin_collection', :label=>'Administrative Collection')
+      t.activity(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                 :path=>'admin_activity', :label=>'Administrative Activity')
+
+      t.embargo(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                 :path=>'embargo', :label=>'Embargo')
+      t.embargo_date(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                :path=>'embargo', :label=>'Embargo Date')
+      t.embargo_condition(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                :path=>'embargo', :label=>'Embargo Condition')
+      t.access_condition(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                :path=>'embargo', :label=>'Access Condition')
+
+      t.copyright(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                         :path=>'embargo', :label=>'Copyright')
+      t.material_type(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                         :path=>'embargo', :label=>'Material type')
+      t.availability(:type => :string, :index_as=>[:stored_searchable, :displayable, :sortable],
+                         :path=>'availability', :label=>'Availability')
+    end
+  end
+end
