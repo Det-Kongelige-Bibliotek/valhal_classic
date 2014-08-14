c4a7c5f9d7859d5f740d0a822dc8e5b7bfc0e8a4
davidgrove73 <z9m6ae7P>
2014-08-14 16:21:56 +0200

    added job to search for new volumes

--- app/jobs/NewLettersJob.rb
+++
@@ -1,9 +0,0 @@
-class NewLettersJob
-  @queue = :brev_ingest
-
-  def self.perform(xmlpath,pdfpath) 
-        File.open('test_file.log', 'w') do |f|
-      	    f.puts "looking for new xml_file in #{xmlpath}"
-    	end
-  end
-end
