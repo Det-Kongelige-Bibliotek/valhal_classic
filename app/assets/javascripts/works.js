9b5e49b66341ec88486959c11ec8b5206bae8415
jatr <jatr@kb.dk>
2014-08-19 15:47:44 +0200

    Parameterise function to take the work id

--- app/assets/javascripts/works.js
+++ app/assets/javascripts/works.js
@@ -74,10 +74,14 @@ function addAgent(relationshipType, agentName, agentObjectID) {
     }
 }
 
-function enableMaterialTypes() {
+/**
+ * Function for populating the adminMetaData Material Type drop-down with data depending on the material group chosen
+ * Makes an AJAX GET request to the WorksController to load required data from admin_metadata_materials.yml
+ */
+function enableMaterialTypes(work_id) {
     //var div =$("#material_type_div");
 
-    $.ajax({url:"/works/valhal:138/get_admin_material_types", format: 'json', success: function(result) {
+    $.ajax({url:"/works/" + work_id +"/get_admin_material_types", format: 'json', success: function(result) {
         console.log(result);
 
         var selected_material_group = $('#material_group').val().toLowerCase();
