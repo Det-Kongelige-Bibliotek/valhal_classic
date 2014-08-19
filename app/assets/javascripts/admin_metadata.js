d76f189b0fd6cc43143e01835b62de27faf0427a
jatr <jatr@kb.dk>
2014-08-19 16:00:31 +0200

    Rename parameter variable to something more generic

--- app/assets/javascripts/admin_metadata.js
+++ app/assets/javascripts/admin_metadata.js
@@ -1,11 +1,12 @@
 /**
  * Function for populating the adminMetaData Material Type drop-down with data depending on the material group chosen
  * Makes an AJAX GET request to the WorksController to load required data from admin_metadata_materials.yml
+ * @param object_id - the Fedora ID of the object whose admin metadata is being changed
  */
-function enableMaterialTypes(work_id) {
+function enableMaterialTypes(object_id) {
     //var div =$("#material_type_div");
 
-    $.ajax({url:"/works/" + work_id +"/get_admin_material_types", format: 'json', success: function(result) {
+    $.ajax({url:"/works/" + object_id +"/get_admin_material_types", format: 'json', success: function(result) {
         console.log(result);
 
         var selected_material_group = $('#material_group').val().toLowerCase();
