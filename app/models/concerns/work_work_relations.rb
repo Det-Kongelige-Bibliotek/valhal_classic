a35d1acf3567decd440731e771db8c78db1f2ce0
Ronan McHugh <mchugh.r@gmail.com>
2014-08-21 13:27:54 +0200

    relationship between letter and scanned work.

--- app/models/concerns/work_work_relations.rb
+++ app/models/concerns/work_work_relations.rb
@@ -6,6 +6,20 @@ module Concerns
     included do
       has_many :nextInSequence, :class_name => 'ActiveFedora::Base', :property=>:next_in_sequence, :inverse_of => :previous_in_sequence
       belongs_to :previousInSequence, :class_name => 'ActiveFedora::Base', :property=>:previous_in_sequence, :inverse_of => :next_in_sequence
+      has_many :parts, :class_name => 'Work', property: :has_parts, inverse_of: :is_part_of
+      belongs_to :is_part_of, :class_name => 'Work', property: :is_part_of, inverse_of: :parts
+
+
+      # Add part and ensure relationship
+      # is defined on both elements.
+      # @param Work
+      # @return Boolean
+      def add_part(part)
+        self.parts << part
+        part.save unless part.pid
+        part.is_part_of = self
+        self.save
+      end
     end
 
   end
--- spec/models/integration/work_work_relations_spec.rb
+++ spec/models/integration/work_work_relations_spec.rb
@@ -10,13 +10,28 @@ describe "Relationships between works" do
 
     it "should be possible to define the next work in sequence with a corresponing previous in sequence" do
       @work2.previousInSequence = @work1
-      @work1.nextInSequence << @work2;
+      @work1.nextInSequence << @work2
       @work1.save!
       @work1.reload
       @work2.reload
 
       @work1.nextInSequence.should == [@work2]
       @work2.previousInSequence.should == @work1
+    end
+
+    it 'should be possible to define a work as part of another work' do
+      master_work = Work.new(title: 'containing work')
+      master_work.save
+      master_work.add_part(@work1)
+      master_work.add_part(@work2)
+      master_work.reload
+      master_work.parts.should be_an Array
+      master_work.parts.should include @work1
+      master_work.parts.should include @work2
+
+      @work1.is_part_of.should be_a Work
+      @work1.is_part_of.should eql master_work
+      @work2.is_part_of.should eql master_work
     end
   end
 end
