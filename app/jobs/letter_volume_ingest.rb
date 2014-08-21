1638008af9521e3f3089825eca0a29c67758e761
Ronan McHugh <mchugh.r@gmail.com>
2014-08-21 09:46:38 +0200

    make sure workType is defined.

--- app/jobs/letter_volume_ingest.rb
+++ app/jobs/letter_volume_ingest.rb
@@ -70,7 +70,7 @@ class LetterVolumeIngest
       w = existing.first
       Resque.logger.debug "existing work found with PID #{w.pid}"
     else
-      w = Work.new
+      w = Work.new(workType: 'Book')
       w.identifier=([{'displayLabel' => 'sysnum', 'value' => sysnum }])
       w.save
       Resque.logger.debug "no matching work found - work created with PID #{w.pid}"
