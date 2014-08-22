efcc108b0787fec3bb5b5976829d9a1492bb3e84
Ronan McHugh <mchugh.r@gmail.com>
2014-08-22 12:55:55 +0200

    Add test for splitter (set to pending due to it's heaviness)

--- spec/jobs/letter_volume_splitter_spec.rb
+++ spec/jobs/letter_volume_splitter_spec.rb
@@ -1,22 +1,40 @@
 require 'spec_helper'
 
 describe 'perform' do
+
+  before {
+    pending 'These tests are extremely long and should only be run when developing the LetterVolumeSplitter'
+  }
+
   before :all do
     # create a sample work with a ordered instance
     # containing a basic file
-    work = Work.new
+    @work = Work.new
     o = OrderedInstance.new
     bf = BasicFile.new
     @xml_path = Rails.root.join('spec', 'fixtures', 'brev', '001003574_000.xml')
     bf.add_file(File.new(@xml_path), true)
     o.files << bf
-    work.add_instance(o)
+    @work.add_instance(o)
 
-    @content = LetterVolumeSplitter.perform(work.pid, bf.pid)
+    LetterVolumeSplitter.perform(@work.pid, bf.pid)
+  end
+
+  after(:all) do
+    delete_all_objects
   end
 
   it 'should throw an error if called with an invalid work pid' do
     expect { LetterVolumeSplitter.perform('reallynotapid', '')}.to raise_error
+  end
+
+  it 'should create a letter work for each div within the tei file' do
+    letters = Work.find(search_result_work_type_ssi: 'Letter')
+    letters.size.should eql 45
+    l = letters[12]
+    l.is_part_of.should == @work
+    prev = l.previousInSequence
+    prev.nextInSequence.should == [l]
   end
 
 end
@@ -35,20 +53,7 @@ describe 'parse_metadata' do
     data[:sender_name].should eql 'Julius Lange.'
     data[:recipient_name].should eql 'CHR. RICHARD'
     data[:sender_address].should eql 'København'
-    data[:note].should eql 'TIL SAMME'
+    data[:note].should eql({displayLabel: 'noteFromText', value: 'TIL SAMME' })
     data[:needs_attention].should be_true
-  end
-end
-
-
-describe 'transform' do
-
-  before (:all) do
-    file = File.new(Rails.root.join('spec', 'fixtures', 'brev', '001003574_000.xml').to_s)
-    @result = LetterVolumeSplitter.transform(file)
-  end
-
-  it 'should return an xml doc' do
-    @result.should be_a Nokogiri::XML::Document
   end
 end
