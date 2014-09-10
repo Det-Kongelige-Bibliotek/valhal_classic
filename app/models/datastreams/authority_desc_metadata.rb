c123dcb0ad69a48dbdf356aca8c12c9b2dccb9b3
davidgrove73 <z9m6ae7P>
2014-09-10 15:01:22 +0200

    follow up on review

--- app/models/datastreams/authority_desc_metadata.rb
+++ app/models/datastreams/authority_desc_metadata.rb
@@ -9,6 +9,19 @@ module Datastreams
       t.type()
       t.value()
       t.reference()
+
+
+      # Person
+      t.firstName()
+      t.lastName()
+      t.title()
+      t.dateOfBirth()
+      t.dateOfDeath()
+
+
+      # Place
+      t.name()
+
     end
 
     def self.xml_template
