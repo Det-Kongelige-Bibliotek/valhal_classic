a00cf78f54bb82001afc0d5e0d6a8446ae15c65b
Ronan McHugh <mchugh.r@gmail.com>
2014-09-12 13:35:33 +0200

    remove annoying log messages.

--- app/services/http_service.rb
+++ app/services/http_service.rb
@@ -5,13 +5,8 @@ require 'httparty'
 class HttpService
 
   def do_post(uri_string, params)
-
-    logger.debug "Base URl = #{uri_string}"
-    logger.debug "Params = #{params.to_s}"
     response = false
     begin
-      url = URI.parse(uri_string)
-      logger.debug "http start"
       response = HTTParty.post(uri_string, :body => params)
     rescue Exception  => e
       logger.error "do_post failed #{uri_string}: #{e.message}"
