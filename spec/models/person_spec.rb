8798fb6fa99b06126becd927bdc5b34209dc69e5
Ronan McHugh <mchugh.r@gmail.com>
2014-09-16 14:27:16 +0200

    methods to retrieve or create a person object based on a string value
    Conflicts:
    	spec/models/person_spec.rb

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -48,5 +48,25 @@ describe Person do
     end
   end
 
+  describe 'from_string' do
+    it 'should create a new person when no matching person exists' do
+      joyce = Person.from_string('Joyce, James')
+      expect(joyce.firstName).to eql 'James'
+      expect(joyce.lastName).to eql 'Joyce'
+    end
 
+    it 'should find a matching person when that person exists' do
+      joyce = Person.create(firstName: 'James', lastName: 'Joyce')
+      p = Person.from_string('Joyce, James')
+      expect(p.pid).to eql joyce.pid
+    end
+  end
+
+  describe 'person_search_fields' do
+    it 'should return a hash mapping solr fields to values' do
+      fields = Person.person_search_fields({lastName: 'James'})
+      expect(fields.keys.first).to eql 'person_lastName_ssi'
+    end
+  end
 end
+
