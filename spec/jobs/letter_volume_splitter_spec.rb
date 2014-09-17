7f768db7e40283cacfa224eefee7e19ea8b6b881
Ronan McHugh <mchugh.r@gmail.com>
2014-09-17 15:37:41 +0200

    test for page breaks, update other test to use new tei file.

--- spec/jobs/letter_volume_splitter_spec.rb
+++ spec/jobs/letter_volume_splitter_spec.rb
@@ -1,35 +1,6 @@
 require 'spec_helper'
 
-# Note that most test functionality has been moved
-# into parse_letters below - this is because the perform method
-# takes a docxml file which we don't know how to split up.
-# Instead we take a smaller tei file and test the parsing methods
-# using that. For this reason, the perform test is set to pending.
-
 describe 'perform' do
-
-  before {
-    pending 'These tests are extremely long and should only be run when developing the LetterVolumeSplitter'
-  }
-
-  before :all do
-    # create a sample work with a ordered instance
-    # containing a basic file
-    @work = Work.new
-    o = OrderedInstance.new
-    bf = BasicFile.new
-    @xml_path = Rails.root.join('spec', 'fixtures', 'brev', 'small-tei.xml')
-    bf.add_file(File.new(@xml_path), true)
-    o.files << bf
-    @work.add_instance(o)
-
-    LetterVolumeSplitter.perform(@work.pid, bf.pid)
-
-  end
-
-  after :all do
-    delete_all_objects
-  end
 
   it 'should throw an error if called with an invalid work pid' do
     expect { LetterVolumeSplitter.perform('reallynotapid', '')}.to raise_error
@@ -39,11 +10,11 @@ end
 describe 'parse letters' do
 
   before :all do
-    doc = Nokogiri::XML(File.open(Rails.root.join('spec', 'fixtures', 'brev', 'small-tei.xml')))
-    @work = Work.create(title: 'The collected letters of Julius Lange', workType: 'Book')
+    doc = Nokogiri::XML(File.open(Rails.root.join('spec', 'fixtures', 'brev', '001152195_000_tei.xml')))
+    @work = Work.create(title: 'Breve fra Johannes Jørgensen til Vicko Stuckenberg', workType: 'Book')
     LetterVolumeSplitter.parse_letters(doc, @work)
     @letters = Work.find(search_result_work_type_ssi: 'Letter')
-    @letter = Work.find(teiRef_si: 'divid136080').first
+    @letter = Work.find(teiRef_si: 'divid182306').first
   end
 
   after :all do
@@ -55,7 +26,7 @@ describe 'parse letters' do
     l = @letters[1]
     l.reload
     l.workType.should eql 'Letter'
-    l.is_part_of.should == @work
+    expect(l.is_part_of).to be_a Work
     prev = l.previous_work
     prev.next_work.should == l
   end
@@ -69,7 +40,7 @@ describe 'parse letters' do
   it 'should create an authority metadata unit for the recipient' do
     @letter.should_not be_nil
     @letter.hasAddressee.first.should be_an AuthorityMetadataUnit
-    @letter.hasAddressee.first.value.should eql 'CHR. RICHARD'
+    @letter.hasAddressee.first.value.should eql 'Stuckenberg'
   end
 
   it 'should create an authority metadata unit for the sender address' do
@@ -82,23 +53,42 @@ describe 'parse letters' do
   it 'should create a basic file for the relevant tei div' do
     @letter.single_file_instances.length.should eql 1
   end
+
+  it 'should create a jpg ordered instance' do
+    expect(@letter.ordered_instance_types[:jpgs]).to_not be_nil
+  end
 end
 
 describe 'parse_metadata' do
-  it 'should return a hash containing the correct values for a given div' do
-    xml_path = Rails.root.join('spec', 'fixtures', 'brev', 'sort-of-tei.xml')
-    xml = Nokogiri::XML(File.new(xml_path))
-    div = xml.css('text body div').first
-    data = LetterVolumeSplitter.parse_data(div)
-    data.should be_a Hash
-    data[:id].should eql 'divid136080'
-    data[:num].should eql '1'
-    data[:date].should eql '1. stpt. 1864'
-    data[:body].should include 'Kære Richardt!'
-    data[:sender_name].should eql 'Julius Lange.'
-    data[:recipient_name].should eql 'CHR. RICHARD'
-    data[:sender_address].should eql 'København'
-    data[:note].should eql({displayLabel: 'noteFromText', value: 'TIL SAMME' })
-    data[:needs_attention].should be_true
+  before :all do
+    xml_path = Rails.root.join('spec', 'fixtures', 'brev', '001152195_000_tei.xml')
+    @xml = Nokogiri::XML(File.new(xml_path))
   end
+
+  it 'should return a hash containing the correct values for a given div' do
+    div = @xml.css('text body div')[1]
+    @data = LetterVolumeSplitter.parse_data(div, '')
+    @data.should be_a Hash
+    @data[:id].should eql 'divid182659'
+    @data[:num].should eql '2'
+    @data[:date].should eql 'd. 5. Maj 1887.'
+    @data[:body].should include 'Paa Gaden — bag en Rude'
+    @data[:sender_name].should eql 'J. J.'
+    @data[:recipient_name].should eql 'VIGGO STUCKENBERG'
+  end
+
+  it 'should contain data for start page and end page when there is no end page' do
+    div = @xml.css('text body div').first
+    data = LetterVolumeSplitter.parse_data(div, '13')
+    expect(data[:start_page]).to eql '13'
+    expect(data[:end_page]).to eql '13'
+  end
+
+  it 'should contain data for start page and end page when there is an end page' do
+    div = @xml.css('text body div')[1]
+    data = LetterVolumeSplitter.parse_data(div, '13')
+    expect(data[:start_page]).to eql '13'
+    expect(data[:end_page]).to eql '14'
+  end
+
 end
