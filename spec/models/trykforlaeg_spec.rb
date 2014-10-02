bbbcaf2ca146e6b4b4bf93ec0a96358f6742c6d8
jatr <jatr@kb.dk>
2014-10-02 15:02:54 +0200

    Use rspec expectation matchers

--- spec/models/trykforlaeg_spec.rb
+++ spec/models/trykforlaeg_spec.rb
@@ -5,7 +5,7 @@ describe Trykforlaeg do
     it 'should validate presence of ISBN' do
       trykforlaeg = Trykforlaeg.new
       trykforlaeg.isbn = '9781934356371'
-      trykforlaeg.valid? should_not be_falsey
+      expect(trykforlaeg.valid?).to eq true
     end
 
     it 'should be invalid when ISBN is absent' do
@@ -17,7 +17,7 @@ describe Trykforlaeg do
     it 'should validate 10 digit ISBN is correct' do
       trykforlaeg = Trykforlaeg.new
       trykforlaeg.isbn = '1-84356-028-3'
-      trykforlaeg.valid? should_not be_falsey
+      expect(trykforlaeg.valid?).to eq true
     end
 
 
@@ -27,6 +27,12 @@ describe Trykforlaeg do
       trykforlaeg.valid? should_not be_falsey
     end
 
-    it 'should be invalid for an ISBN containing invalid characters'
+    it 'should be invalid for an ISBN containing invalid characters' do
+      trykforlaeg = Trykforlaeg.new
+      trykforlaeg.isbn = 'garbage'
+      expect(trykforlaeg.valid?).to eq false
+      expect(trykforlaeg.errors.size).to be == 1
+      expect(trykforlaeg.errors.messages[:isbn][0]).to be == 'is not a valid ISBN code'
+    end
   end
 end
