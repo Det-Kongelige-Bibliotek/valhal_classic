94394b9e4c752a9e2aa759d637e6abe1ea1431dd
jatr <jatr@kb.dk>
2014-10-03 09:42:01 +0200

    Adding EDTF validation of dateIssued values

--- spec/models/trykforlaeg_spec.rb
+++ spec/models/trykforlaeg_spec.rb
@@ -5,17 +5,20 @@ describe Trykforlaeg do
     it 'should validate presence of ISBN' do
       trykforlaeg = Trykforlaeg.new
       trykforlaeg.isbn = '9781934356371'
+      trykforlaeg.dateIssued = '2014-10-02'
       expect(trykforlaeg.valid?).to eq true
     end
 
     it 'should be invalid when ISBN is absent' do
       trykforlaeg = Trykforlaeg.new
-      trykforlaeg.valid? should_not be_falsey
+      trykforlaeg.dateIssued = '2014-10-02'
+      expect(trykforlaeg.valid?).to eq false
       expect(trykforlaeg.errors.size).to be == 2
     end
 
     it 'should validate 10 digit ISBN is correct' do
       trykforlaeg = Trykforlaeg.new
+      trykforlaeg.dateIssued = '2014-10-02'
       trykforlaeg.isbn = '1-84356-028-3'
       expect(trykforlaeg.valid?).to eq true
     end
@@ -23,16 +26,27 @@ describe Trykforlaeg do
 
     it 'should validate 13 digit ISBN is correct' do
       trykforlaeg = Trykforlaeg.new
+      trykforlaeg.dateIssued = '2014-10-02'
       trykforlaeg.isbn = '9781934356371'
-      trykforlaeg.valid? should_not be_falsey
+      expect(trykforlaeg.valid?).to eq true
     end
 
     it 'should be invalid for an ISBN containing invalid characters' do
       trykforlaeg = Trykforlaeg.new
       trykforlaeg.isbn = 'garbage'
+      trykforlaeg.dateIssued = '2014-10-02'
       expect(trykforlaeg.valid?).to eq false
       expect(trykforlaeg.errors.size).to be == 1
       expect(trykforlaeg.errors.messages[:isbn][0]).to be == 'is not a valid ISBN code'
+    end
+
+    it 'should be invalid for an invalid dateIssued value' do
+      trykforlaeg = Trykforlaeg.new
+      trykforlaeg.dateIssued = '2014-10-0'
+      trykforlaeg.isbn = '9781934356371'
+      expect(trykforlaeg.valid?).to eq false
+      expect(trykforlaeg.errors.size).to be == 1
+      expect(trykforlaeg.errors.messages[:dateIssued][0]).to be == 'date issued is an invalid EDTF format'
     end
   end
 end
