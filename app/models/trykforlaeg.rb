887e1b2ec7d6b8a75704b1d0e7b7435801aaeea5
jatr <jatr@kb.dk>
2014-10-03 09:42:01 +0200

    Adding EDTF validation of dateIssued values

--- app/models/trykforlaeg.rb
+++ app/models/trykforlaeg.rb
@@ -2,9 +2,20 @@ class Trykforlaeg < OrderedInstance
   include Concerns::Instance
   include ActiveRecord::Validations
 
+  validate :date_issued_is_valid_edtf
+
   has_metadata :name => 'descMetadata', :type => Datastreams::InstanceDescMetadata
   has_attributes :isbn, datastream: 'descMetadata', multiple: false
 
   validates :isbn, :presence => true
   validates :isbn, :isbn_format => true
+
+  validates :dateIssued, :presence => true
+
+  private
+  def date_issued_is_valid_edtf
+    if (EDTF.parse self.dateIssued).nil?
+      errors.add(:dateIssued, 'date issued is an invalid EDTF format')
+    end
+  end
 end
